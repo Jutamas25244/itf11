967111b56d3504919d25648b743545dea1e92cee
Jutamas25244 <73680085+Jutamas25244@users.noreply.github.com>
2020-10-30 11:02:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello world";
-?>
+<html>
+    <head>
+        <title>WEBSITE</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <center><h1>ประวัติส่วนร่วม</h1>
+        <p>ชื่อ : นางสาว จุฑามาศ   ชนาฐิติกุล</p>
+        <p>ชื่อเล่น : ลูกตาล</p>
+        <img src="cat.jpg" width="300px" hight="300px"><br>
+        <img src="cat1.jpg" width="600px" hight="500px">
+        <br>
+        <a href="https://anime-hayai.com/">Anime</a>
+        <br>
+        <a href="https://mangakakalot.com/">Manga</a>
+
+        <h2>เรื่องเล่า เช้านี้!!!</h2>
+        <p>เรื่องมีอยู่ว่า</p>
+        <h5>ว่าาาาา !!!!!</h5>
+        <h4>ว่าาาา !!!!</h4>
+        <head>ว่าาา !!!</head>
+        <p>เด็กคนหนึ่งตื่นขึ้นมาตอนเช้า แต่เขายังอยากจะนอนต่อและเขาก็ขี้เกียจมาเรียน เขาเลยนอนต่อซะเลย </p>
+        <h1>จบแล้วจ้าาา</h1>
+    </body>
+</html>
